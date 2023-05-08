d93032419340a88d7e9b740b5a20adc86e5c5a1b
Steph Symosko <steph.symosko@gmail.com>
2023-05-07 20:32:17 -0400

    adding router navigation, updating tsconfig to not strict

--- stephsymosko/apps/angular-app/src/app/app.module.ts
+++ stephsymosko/apps/angular-app/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent, ProductAlertsComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, ProductListComponent, ProductAlertsComponent, ProductDetailsComponent],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+      { path: 'products/:productId', component: ProductDetailsComponent}
+    ])
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ stephsymosko/apps/angular-app/src/app/product-details/product-details.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product, products } from '../../simply-wreaths-products';
+
+
+@Component({
+  selector: 'stephsymosko-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.scss']
+})
+export class ProductDetailsComponent implements OnInit {
+
+  product: Product | undefined;
+
+  constructor( private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    // first get the product id from the current route
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+
+    // find the product that corresponds with the id provided in route
+    this.product = products.find(product => product.id === productIdFromRoute);
+  }
+
+}
